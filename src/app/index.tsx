45dc0633a9c3e64819070de6172ab4d5a962d118
iyed8 <164577765+iyedGrs@users.noreply.github.com>
2025-01-18 13:53:13 +0100

    Co-authored-by: ArijTahri <ArijTahri@users.noreply.github.com> Co-authored-by: youssefbyoussef <youssefbyoussef@users.noreply.github.com>

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,9 @@
 import { Link, Redirect } from "expo-router";
 import React from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
-// import "../global.css";
+
 const index = () => {
   return <Redirect href="/auth/login" />;
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
 
 export default index;
